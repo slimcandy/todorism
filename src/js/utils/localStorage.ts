58c6ac9ad4325bb0541c79aa72f6e5a632c3ce5b
dudanova <dudanova@protei-lab.ru>
2022-06-23 16:18:16 +0300

    small fix in localStorage, fetchStorage

--- src/js/utils/localStorage.ts
+++ src/js/utils/localStorage.ts
@@ -4,8 +4,7 @@ export const pullLocalStorage = async (
   localStorageKey: string
 ): Promise<string | null> => {
   try {
-    // eslint-disable-next-line @typescript-eslint/await-thenable
-    const item = await window.localStorage.getItem(localStorageKey);
+    const item = window.localStorage.getItem(localStorageKey);
     return item && item.length > 0 ? item : null;
   } catch (error) {
     return null;
@@ -21,7 +20,7 @@ export const pushLocalStorage = async (
 export const getLocalList = async (key: string): Promise<List | null> => {
   try {
     const item = await pullLocalStorage(key);
-    return item ? JSON.parse(item) as List: null;
+    return item ? (JSON.parse(item) as List) : null;
   } catch (error) {
     return null;
   }
