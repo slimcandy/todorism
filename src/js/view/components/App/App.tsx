5b823cde9cdac9b1ebfea64b47b00615fcbb10cf
dudanova <dudanova@protei-lab.ru>
2022-06-27 19:14:06 +0300

    create deNavPage and routes for app navigation

--- src/js/view/components/App/App.tsx
+++ src/js/view/components/App/App.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store from "../../../stores/store";
-import Panel from "../Panel/Panel";
+import { Panel } from "../Panel/Panel";
 import { UIKitPage } from "../UIKitPage/UIKitPage";
+import { DevNavPage } from "../DevNavPage/DevNavPage";
 
 import "../../../../styles/index.css";
 
-function App() {
+export function App() {
   return (
     <Provider store={store}>
       <main>
-        <Panel />
-        <UIKitPage />
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<DevNavPage/>}/>
+              <Route path="/alex" element={<Panel />}/>
+              <Route path="/ui-kit" element={ <UIKitPage />} />
+            </Routes>
+        </BrowserRouter>
       </main>
     </Provider>
   );
 }
-
-export default App;
