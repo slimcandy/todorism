c7ecfdd80ee0fbf17ee63e7d8a474090d3f59489
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-07-07 11:03:36 +0300

    Fix up to design

--- src/js/view/components/SearchBar/SearchBar.tsx
+++ src/js/view/components/SearchBar/SearchBar.tsx
@@ -10,13 +10,13 @@ const SearchBar = (props: SearchBarProps) => {
         className={`relative text-todo-dark-2 bg-todo-black-2 hover:text-todo-dark-3 focus-within:placeholder:text-todo-dark-3 ${className}`}
       >
         <ZoomIcon
-          className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3 "
+          className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-4"
           size={20}
         />
         <input
           type="search"
           placeholder={placeholder}
-          className="input w-full max-w-xs text-current pl-14 bg-todo-black-2 text-white placeholder:text-todo-dark-2 hover:placeholder:text-todo-dark-3 focus:placeholder:text-white"
+          className="input w-full max-w-xs text-current pl-12 h-9 bg-todo-black-2 text-white placeholder:text-todo-dark-2 hover:placeholder:text-todo-dark-3 focus:placeholder:text-white"
           onChange={onChange}
           value={value}
         />
