d14a147a3812e32b3738dc24f084f2574a11427c
dudanova <dudanova@protei-lab.ru>
2022-07-11 13:53:36 +0300

    create components

--- src/js/view/elements/ typo/TitleH.tsx
+++ src/js/view/elements/ typo/TitleH.tsx
@@ -1,17 +1,19 @@
 import React, { PropsWithChildren } from "react";
 
 type TitleHProps = {
-  hLevel: number
-  children: PropsWithChildren<string>
+  hLevel: number;
+  children: PropsWithChildren<string>;
   // eslint-disable-next-line react/require-default-props
-  className?: string | undefined
-}
+  className?: string | undefined;
+};
 
 export const TitleH = (props: TitleHProps) => {
   const { hLevel, children, className = "" } = props;
 
   return (
     // TODO: remove "as string"
-    <h1 className={`text-h${hLevel.toString()} font-bold ${className}`}>{children}</h1>
-  )
-}
+    <h1 className={`text-h${hLevel.toString()} font-bold ${className}`}>
+      {children}
+    </h1>
+  );
+};
