9b619602cf9eadbd8451b6176cc793d494e5110e
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-07-12 18:38:18 +0300

    Fix icon color

--- src/js/view/components/SearchBar/SearchBar.tsx
+++ src/js/view/components/SearchBar/SearchBar.tsx
@@ -7,7 +7,7 @@ const SearchBar = (props: SearchBarProps) => {
   return (
     <form onSubmit={onSubmit}>
       <label
-        className={`relative text-dark-2 bg-black-2 hover:text-dark-3 focus-within:placeholder:text-dark-3 ${className}`}
+        className={`relative text-dark-2 bg-black-2 hover:text-dark-3 focus-within:text-dark-3 focus-within:placeholder:text-dark-3 ${className}`}
       >
         <ZoomIcon
           className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-4"
