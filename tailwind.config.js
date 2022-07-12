347b5b7a15792588f92d2866b27aa2e52d2fcc56
dudanova <dudanova@protei-lab.ru>
2022-07-12 20:49:23 +0300

    remove my-tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [require("daisyui"), require('@tailwindcss/typography'),],
-  tailwindcss: {config: "./my-tailwind.config.js"}
+  plugins: [require("daisyui"), require('@tailwindcss/typography')],
+  theme: {
+    extend: {
+      fontSize: {
+        "h1": ["1.5rem", { lineHeight: "32px" }],
+        "h2": ["1.125rem", { lineHeight: "22px" }],
+        "h3": ["1rem", { lineHeight: "22px" }],
+        "medium": ["0.875rem", { lineHeight: "18px" }],
+        "small": ["0.75rem", { lineHeight: "16px" }]
+      },
+    }
+  },
 }
