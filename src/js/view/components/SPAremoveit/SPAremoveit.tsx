44f36e4cc12976fb4ddd8807d3e07e7ae5f055b5
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-07-12 18:15:40 +0300

    Add add items form

--- src/js/view/components/SPAremoveit/SPAremoveit.tsx
+++ src/js/view/components/SPAremoveit/SPAremoveit.tsx
@@ -99,7 +99,25 @@ const SPAremoveit = () => {
     })
       .then(console.log)
       .catch(console.error)
-      .finally(() => setSuccessPageOpen(true));
+      .finally(() => setSuccessPageOpen(true)); // тут и ссылка на мероприятие будет
+  };
+
+  // 5. Список вещей
+  const [itemListTitle, setItemListTitle] = useState<string>("");
+  const [itemListItems, setItemListItems] = useState<string[]>([]);
+
+  const handleItemListTitleChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setItemListTitle(event.target.value);
+  };
+
+  const handleItemListFormSubmit = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    setItemListItems([...itemListItems, itemListTitle]);
+    setItemListTitle("");
   };
 
   return (
@@ -214,6 +232,49 @@ const SPAremoveit = () => {
           <li>Ура! Мероприятие создано! Вот ссылка: урл.рус</li>
         ) : (
           "Добавьте людей, чтобы скопировать ссылку"
+        )}
+        {successPageOpen && (
+          <li>
+            <h2>5. Список вещей</h2>
+            {itemListItems.length > 0 && (
+              <ul>
+                {itemListItems.map((item) => (
+                  <li>{item}</li>
+                ))}
+              </ul>
+            )}
+            <form
+              className="flex my-4 justify-center w-full gap-2"
+              onSubmit={handleItemListFormSubmit}
+            >
+              <label htmlFor="title" className="sr-only">
+                Title
+              </label>
+              <input
+                id="title"
+                type="text"
+                value={itemListTitle}
+                onChange={handleItemListTitleChange}
+                placeholder="Milk"
+                className="input input-bordered w-full max-w-xs"
+                data-test="create-list-form-input"
+              />
+              <button type="submit" className="btn btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="h-6 w-6"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                </svg>
+                <span className="sr-only">Add</span>
+              </button>
+            </form>
+          </li>
         )}
       </ol>
     </main>
