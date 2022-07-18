449f0c4b93dfee8a73b65122609e8b242df96d3e
dudanova <dudanova@protei-lab.ru>
2022-07-18 14:51:50 +0300

    refactoring

--- src/js/view/components/WelcomePage/WelcomePage.tsx
+++ src/js/view/components/WelcomePage/WelcomePage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { TextBodyLarge, TitleH1 } from "../../elements/typo";
 import { Input } from "../../elements/Input";
-import tentImg from "../../../../assets/images/tent.png"
+import tentImg from "../../../../assets/images/tent.png";
 import { ButtonPrimary } from "../../elements/buttons";
 
 export const WelcomePage = () => {
-  const placeHolder = "Как вас зовут?"
+  const placeHolder = "Как вас зовут?";
   return (
     <div className="pt-16 pb-6 px-4 h-full text-center">
       <div className="mb-16">
@@ -15,17 +15,19 @@ export const WelcomePage = () => {
         <TitleH1>Добро пожаловать!</TitleH1>
       </div>
       <div className="mb-4">
-        <TextBodyLarge >Мы — приложение [NAME], лучшее приложение для организации мероприятий!</TextBodyLarge>
+        <TextBodyLarge>
+          Мы — приложение [NAME], лучшее приложение для организации мероприятий!
+        </TextBodyLarge>
       </div>
       <div className="mb-10">
         <TextBodyLarge>Как вас зовут?</TextBodyLarge>
       </div>
       <div className="mb-20">
-        <Input type="text" placeholder={placeHolder}/>
+        <Input type="text" placeholder={placeHolder} />
       </div>
       <div className="px-7">
         <ButtonPrimary>Запомнить меня</ButtonPrimary>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
--- src/js/view/elements/Input.tsx
+++ src/js/view/elements/Input.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 
 type InputProps = {
-  type: string
-  title?: string
-  placeholder?: string
-}
+  type: string;
+  title?: string;
+  placeholder?: string;
+};
 
 export const Input = (props: InputProps) => {
   const { type, title = "", placeholder = "" } = props;
   return (
     <label className="block">
-      {title &&
+      {title && (
         <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
           {title}
-        </span>}
-      <input type={type} name={type}
-             className="mt-1 px-3 py-2 bg-white border shadow-sm
+        </span>
+      )}
+      <input
+        type={type}
+        name={type}
+        className="mt-1 px-3 py-2 bg-white border shadow-sm
                  border-slate-300 placeholder-slate-400
                  focus:outline-none focus:border-sky-500 focus:ring-sky-500
                  block w-full rounded-md sm:text-sm focus:ring-1
                  dark:bg-black-2 border-none outline-none"
-             placeholder={placeholder} />
+        placeholder={placeholder}
+      />
     </label>
   );
 };
