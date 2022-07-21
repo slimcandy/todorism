b45316d21ecb4b707a18e14d0c16f2a42b5ca998
dudanova <dudanova@protei-lab.ru>
2022-07-21 12:36:12 +0300

    refactoring

--- src/js/view/components/NoEventsPage/NoEventsPage.tsx
+++ src/js/view/components/NoEventsPage/NoEventsPage.tsx
@@ -5,17 +5,17 @@ import fireImg from "../../../../assets/images/fire.png";
 import "./NoEventsPage.css";
 
 export const NoEventsPage = () => (
-  <div className="no-events-page
+  <div
+    className="no-events-page
    flex flex-col
    justify-between
     px-4 pt-3 pb-6
     md:w-80 w-full
-    text-black-0 dark:text-light-4">
-
+    text-black-0 dark:text-light-4"
+  >
     <TitleH1>Ваши мероприятия</TitleH1>
 
     <div className="place-items-center">
-
       <div className="mb-16 mx-auto w-48 text-light-0 dark:text-green-0">
         <EllipseWithImg imgSrc={fireImg} />
       </div>
@@ -23,7 +23,6 @@ export const NoEventsPage = () => (
       <div className="text-center">
         <TitleH1>У вас еще нет мероприятий</TitleH1>
       </div>
-
     </div>
 
     <div className="px-7">
