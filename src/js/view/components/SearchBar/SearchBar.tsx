87c06e170b423c8225b7e2e233047c99810de37e
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-07-21 12:35:04 +0300

    Fix height

--- src/js/view/components/SearchBar/SearchBar.tsx
+++ src/js/view/components/SearchBar/SearchBar.tsx
@@ -24,7 +24,7 @@ const SearchBar = (props: SearchBarProps) => {
         <input
           type="search"
           placeholder={placeholder}
-          className={`input w-full max-w-xs text-current pl-12 h-9 bg-black-2 text-white placeholder:text-dark-2 hover:placeholder:text-dark-3 focus:placeholder:text-white disabled:text-black-3 disabled:bg-black-2 disabled:border-none disabled:placeholder:text-black-3 ${styles.search} `}
+          className={`input w-full max-w-xs text-current pl-12 h-11 bg-black-2 text-white placeholder:text-dark-2 hover:placeholder:text-dark-3 focus:placeholder:text-white disabled:text-black-3 disabled:bg-black-2 disabled:border-none disabled:placeholder:text-black-3 ${styles.search} `}
           onChange={onChange}
           value={value}
           disabled={disabled}
