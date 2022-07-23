a522cd01a99092147fbcd7f8ab03af00a6de7fe5
ldudanova <cobalt.amadeus@gmail.com>
2022-07-23 20:33:53 +0300

    Formating

--- src/js/view/elements/TagMe.tsx
+++ src/js/view/elements/TagMe.tsx
@@ -3,13 +3,15 @@ import { useTranslation } from "react-i18next";
 import { TextBodySmall } from "./typography";
 
 export const TagMe = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div className="flex justify-center text-center 
+  return (
+    <div
+      className="flex justify-center text-center 
         rounded-full bg-green-1 dark:bg-green-0
-        text-light-4 h-4 w-4">
-            <TextBodySmall>{t("me")}</TextBodySmall>
-        </div>
-    )
-}
+        text-light-4 h-4 w-4"
+    >
+      <TextBodySmall>{t("me")}</TextBodySmall>
+    </div>
+  );
+};
