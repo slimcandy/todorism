fbdf382de26c8c7eb99d3deec975c70a78002b29
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-08-17 18:53:19 +0300

    Prettify code

--- src/js/common/constants.ts
+++ src/js/common/constants.ts
@@ -4,7 +4,7 @@ export const urlApi = `${url}/api/`;
 export const urlApiGet = `${urlApi}v1/get-`;
 export const urlApiSet = `${urlApi}v1/set-`;
 export const localStorageUsernameKey = "nickname";
-export const localStorageTripIdKey = "trip_ids";
+export const localStorageTripObjects = "trip_objects";
 
 // Elements
 export const langLocales = [
