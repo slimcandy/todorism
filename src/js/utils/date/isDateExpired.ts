eb7077d0fd960f2b288fc504fbe2a2ea91388046
romalama <110817562+romalama@users.noreply.github.com>
2022-09-15 00:16:31 +0300

    Feature/task 80 (#35)
    * task-80: change behaviour of linters for staged files
    
    * task-80: add cross-env
    
    * task-80: add dayjs
    
    * task-80: update date utils methods

--- src/js/utils/date/isDateExpired.ts
+++
@@ -1,6 +0,0 @@
-export const isDateExpired = (date: Date | undefined) => {
-  if (!date) return false;
-  const today = new Date().valueOf();
-  const checkingDate = new Date(date).valueOf();
-  return today < checkingDate;
-};
