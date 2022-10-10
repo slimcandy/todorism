96f7465144fa3a935a8a23772fc0a57db69de3d6
RSulyaev <dev.sulyaevrs@gmail.com>
2022-10-10 19:41:46 +0300

    task-100: add msw for api in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 import "!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+import { saveCurrentEventInLocalStorage } from "../src/js/utils/localStorage";
+
+// Initialize user data
+saveCurrentEventInLocalStorage({
+  member_uid: "39847088-6223-44d1-9874-62e1ad1e3277",
+  trip_uid: "b1685c01-8f84-499c-a59f-ffbec4d34bd3",
+});
+
+// Initialize MSW
+initialize();
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +20,5 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [mswDecorator];
