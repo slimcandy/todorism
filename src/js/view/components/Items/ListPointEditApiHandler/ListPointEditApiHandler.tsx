aff8e2a3fc2441abf7f722096c3f97e007049501
romalama <110817562+romalama@users.noreply.github.com>
2022-11-11 00:55:38 +0300

    task-117: Integrate ListPointEditForm with recommended/private/common lists (#43)
    * task-110: add TagsGroup component
    
    * task-115: add Loader component
    
    * task-115: move Loader import to components
    
    * task-115: update loader sizing
    
    * task-114: add ActionPanel and PageWrapper components
    
    * task-114: add PageWrapper import to components/index
    
    * task-100: add msw for api in storybook
    
    * task-110: add i18next to storybook
    
    * task-110: add ListPointEditForm
    
    * task-117: add RecommendedListPointEdit, ListPointEditApiHandler components
    
    * task-114: remove wrapped div
    
    * task-110: add type for useState
    
    * task-110: add keyPress handler for component Counter
    
    * task-110: add key for Select options and TagsGroup
    
    * task-110: fix count type for ListPoint
    
    * task-117: merge updated ListPointEditForm
    
    * task-115: update Loading component realization
    
    * task-117: merge loader
    
    * task-117: add accessIds

---
+++ src/js/view/components/Items/ListPointEditApiHandler/ListPointEditApiHandler.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { IListPointEditApiHandler } from "./ListPointEditApiHandlerProps";
+import { IListPoint } from "../../../../interfaces";
+import { ListPointEdit } from "../ListPointEdit/ListPointEdit";
+
+import {
+  editPrivateListPoint,
+  editCommonListPoint,
+} from "../../../../api_clients";
+import { useLoading } from "../../../../hooks";
+
+export const ListPointEditApiHandler = (props: IListPointEditApiHandler) => {
+  const { accessIds, listPoint, listPointIndex, listPointType } = props;
+
+  const { setLoading } = useLoading();
+
+  const isCreationMode = listPointIndex === undefined;
+
+  const changeListPoints = async (editedListPoint: IListPoint) => {
+    try {
+      const { trip_uid, member_uid } = accessIds;
+
+      if (trip_uid && member_uid) {
+        setLoading(true);
+
+        const mode: "add" | "edit" = isCreationMode ? "add" : "edit";
+        const listPointData = {
+          mode,
+          tripUid: trip_uid,
+          listPoint: editedListPoint,
+          memberUid: member_uid,
+        };
+
+        if (listPointType === "common") {
+          await editCommonListPoint(listPointData);
+        } else if (listPointType === "private") {
+          await editPrivateListPoint(listPointData);
+        }
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <ListPointEdit
+      listPoint={listPoint}
+      isCreationMode={isCreationMode}
+      onClick={(editedListPoint) => {
+        void changeListPoints(editedListPoint);
+      }}
+    />
+  );
+};
