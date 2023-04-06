f87d48dc6769e6bbd24e9d6c916fd7fc514e5768
RSulyaev <dev.sulyaevrs@gmail.com>
2023-04-06 19:19:46 +0300

    task-125: fix cursor pointer in list item

--- src/js/view/elements/tags/TagProps.ts
+++ src/js/view/elements/tags/TagProps.ts
@@ -3,6 +3,7 @@ import React from "react";
 export interface TagProps {
   children: string;
   isActive?: boolean;
+  isButton?: boolean;
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
